1721e68e1e4c426f5c408b494f1005bc1ef9a573
Stephen Appiah Frimpong <eunisaesea@gmail.com>
2017-03-03 23:52:03 +0100

    Final Projecte

--- JavaScript/parallax.js
+++ JavaScript/parallax.js
@@ -1,11 +1,30 @@
-$(document).ready(function(){
-  $('section[data-type="parallax_section"]').each(function(){
-    var $bgobj = $(this);
-    $(window).scroll(function() {
-        $window = $(window);
-        var yPos = -($window.scrollTop() / $bgobj.data('speed'));
-        var coords = '75% '+ yPos + 'px';
-        $bgobj.css({ backgroundPosition: coords });
+//$(document).ready(function(){
+//  $('section[data-type="parallax_section"]').each(function(){
+//    var $bgobj = $(this);
+//    $(window).scroll(function() {
+//        $window = $(window);
+//        var yPos = -($window.scrollTop() / $bgobj.data('speed'));
+//        var coords = '75% '+ yPos + 'px';
+//        $bgobj.css({ backgroundPosition: coords });
+//    });
+//  });
+//});
+
+
+window.onload = ()=>{
+    
+    let posY;
+    let imatge = document.querySelectorAll('#paralax');
+    
+    imatge.forEach(function(element , index) {
+        
+        window.addEventListener('scroll',function(){
+            posY = window.scrollY;
+            element.style.top =  posY * 0.5+"px";
+        });
+        
     });
-  });
-});
+
+    
+    
+}
